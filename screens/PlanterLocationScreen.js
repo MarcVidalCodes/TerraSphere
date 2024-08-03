3aed3234ec775d5c4acca60e5e1feb2d5f1661bb
MarcVidalCodes <marcgavvid@gmail.com>
2024-08-03 00:59:12 -0400

    early file structure, need to add navbar

---
+++ screens/PlanterLocationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PlanterLocationScreen = () => {
+    return (
+        <View>
+            <Text>Planter Location Screen</Text>
+        </View>
+    );
+};
+
+export default PlanterLocationScreen;
---
+++ screens/RedeemScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RedeemScreen = () => {
+    return (
+        <View>
+            <Text>Redeem Screen</Text>
+        </View>
+    );
+};
+
+export default RedeemScreen;
