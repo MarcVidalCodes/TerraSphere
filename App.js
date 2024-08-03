4aef3b4efbf82ff2e4e5031633f9b060bac5a81b
MarcVidalCodes <marcgavvid@gmail.com>
2024-08-03 10:38:07 -0400

    redid app.js to work with redux toolkit; put contents inside Provider component

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store'; // Ensure the path to your store is correct
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome6';
-import IonIcon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './screens/HomeScreen';
 import PlanterLocationScreen from './screens/PlanterLocationScreen';
 import CameraScreen from './screens/CameraScreen';
@@ -30,55 +31,57 @@ function HomeStack() {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaProvider>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-          style={{ flex: 1 }}
-          keyboardVerticalOffset={Platform.OS === "ios" ? -30 : 0}
-        >
-          <Tab.Navigator
-            initialRouteName="Home"
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-
-                if (route.name === 'Green Snapper') {
-                  iconName = focused ? 'camera' : 'camera-outline';
-                  return <IonIcon name={iconName} size={size} color={color} />;
-                } else if (route.name === 'Home') {
-                  iconName = focused ? 'home' : 'home-outline';
-                  return <IonIcon name={iconName} size={size} color={color} />;
-                } else if (route.name === 'Planter Mapper') {
-                  iconName = focused ? 'map' : 'map-outline';
-                  return <IonIcon name={iconName} size={size} color={color} />;
-                } else if (route.name === 'Community') {
-                  iconName = focused ? 'people-group' : 'people-group';
-                  return <Icon name={iconName} size={size} color={color} />;
-                } else if (route.name === 'Redeem') {
-                  iconName = focused ? 'cash' : 'cash-outline';
-                  return <IonIcon name={iconName} size={size} color={color} />;
-                }
-
-                return null;
-              },
-              headerShown: false,
-              tabBarActiveTintColor: '#059212',
-              tabBarInactiveTintColor: 'gray',
-              tabBarStyle: {
-                display: 'flex'
-              }
-            })}
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -30 : 0}
           >
-            <Tab.Screen name="Green Snapper" component={CameraScreen} />
-            <Tab.Screen name="Community" component={CommunityScreen} />
-            <Tab.Screen name="Home" component={HomeStack} />
-            <Tab.Screen name="Redeem" component={RedeemScreen} />
-            <Tab.Screen name="Planter Mapper" component={PlanterLocationScreen} />
-          </Tab.Navigator>
-        </KeyboardAvoidingView>
-      </SafeAreaProvider>
-    </NavigationContainer>
+            <Tab.Navigator
+              initialRouteName="Home"
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+
+                  if (route.name === 'Green Snapper') {
+                    iconName = focused ? 'camera' : 'camera-outline';
+                    return <Icon name={iconName} size={size} color={color} />;
+                  } else if (route.name === 'Home') {
+                    iconName = focused ? 'home' : 'home-outline';
+                    return <Icon name={iconName} size={size} color={color} />;
+                  } else if (route.name === 'Planter Mapper') {
+                    iconName = focused ? 'map' : 'map-outline';
+                    return <Icon name={iconName} size={size} color={color} />;
+                  } else if (route.name === 'Community') {
+                    iconName = focused ? 'people' : 'people-outline';
+                    return <Icon name={iconName} size={size} color={color} />;
+                  } else if (route.name === 'Redeem') {
+                    iconName = focused ? 'cash' : 'cash-outline';
+                    return <Icon name={iconName} size={size} color={color} />;
+                  }
+
+                  return null;
+                },
+                headerShown: false,
+                tabBarActiveTintColor: '#059212',
+                tabBarInactiveTintColor: 'gray',
+                tabBarStyle: {
+                  display: 'flex'
+                }
+              })}
+            >
+              <Tab.Screen name="Green Snapper" component={CameraScreen} />
+              <Tab.Screen name="Community" component={CommunityScreen} />
+              <Tab.Screen name="Home" component={HomeStack} />
+              <Tab.Screen name="Redeem" component={RedeemScreen} />
+              <Tab.Screen name="Planter Mapper" component={PlanterLocationScreen} />
+            </Tab.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
